d7947834840acc2a1cbf3a0f233460877711c385
Darren Ford <dford_au-reg@yahoo.com>
2018-07-31 07:58:38 +1000

    Initial Commit

---
+++ Mounting Point/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Mounting Point
+//
+//  Created by Darren Ford on 31/7/18.
+//  Copyright © 2018 Darren Ford. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
